f3d72f58c29e58a7eb1f678d5caa0dce213b8d60
birdaaron <735414905@163.com>
2020-11-06 18:25:23 +0800

    <feat>完成首页界面，添加了底部导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,13 +7,16 @@ Page({
     bannerData : ["/image/banner1.jpg","/image/banner2.jpg","/image/banner3.jpg"],
     btnLoginStr : "登录",
     nearestCanteen :"",
-    isLogin : false,
-    userInfo : {}
+    nearestCanteenDistance :0,
+    time:"",
   },
   onLoad: function(options)
   {
     var _this = this;
     _this.findNearestCanteen();
+    _this.setData({
+        time : new Date().toLocaleTimeString().substring(0,2)
+    });
     
   },
   findNearestCanteen() 
@@ -22,13 +25,17 @@ Page({
     wx.getLocation({
       success : function(res)
       {
-         var distance1 = _this.getDistance(res.latitude,res.longitude,113.401579,23.050244);
-         var distance2 = _this.getDistance(res.latitude,res.longitude,113.399365,23.04839);
-         var distance3 = _this.getDistance(res.latitude,res.longitude,113.394487,23.048399);
-         var distance4 = _this.getDistance(res.latitude,res.longitude,113.390418,23.048511);
-         var max = Math.max(distance1,distance2,distance3,distance4);
+        console.log(res.latitude);
+        
+        console.log(res.longitude);
+
+         var distance1 = _this.getDistance(res.latitude,res.longitude,23.050244,113.401579);
+         var distance2 = _this.getDistance(res.latitude,res.longitude,23.04839,113.399365);
+         var distance3 = _this.getDistance(res.latitude,res.longitude,23.048399,113.394487);
+         var distance4 = _this.getDistance(res.latitude,res.longitude,23.048511,113.390418);
+         var min = Math.min(distance1,distance2,distance3,distance4);
          var result = "";
-         switch(max)
+         switch(min)
          {
            case distance1:
             result = "一饭";
@@ -44,7 +51,8 @@ Page({
             break;
          }
         _this.setData({
-          nearestCanteen:result
+          nearestCanteen:result,
+          nearestCanteenDistance : min.toFixed(0)
         });
       }
     })
@@ -55,7 +63,7 @@ Page({
     var b = (long1-long2)*Math.PI/180.0;
     var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2)+Math.cos(lat1*Math.PI/180.0)*Math.cos(lat2*Math.PI/180.0)*Math.pow(Math.sin(b/2),2)));
     s = s*6378.137;
-    s = Math.round(s*10000)/10000;
+    
     
     return s;
   },
@@ -91,6 +99,6 @@ Page({
     }  
     
   }
- 
+
 
 })
