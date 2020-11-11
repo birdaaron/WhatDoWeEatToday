bcd94697c6b006e9f9773c4ce998ce39a0de8ef6
birdaaron <735414905@163.com>
2020-11-11 21:15:07 +0800

    <feat> 完成自主选餐界面

--- pages/chose/chose.js
+++ pages/chose/chose.js
@@ -3,13 +3,58 @@ Page({
 
   data: {
     tabs : [],
-    currentTab : 0
+    dishesTypeTab :[],
+    dishes : [],
+   
   },
 
-  onLoad: function (options) {
+  onLoad: function (options) 
+  {
+    var _this = this;
     const titles = ['饭堂', '菜品']
     const tabs = titles.map(item => ({title: item}))
-    this.setData({tabs})
+    _this.setData({tabs})
+    wx.request({
+      url: 'http://localhost:8527/api/selectByOneself/',
+      success : function(res)
+      {
+        var typeList = res.data.data
+        var mTypeList = typeList.map(item=>({id:item.id,title:item.name}))
+        _this.setData(
+          {
+            dishesTypeTab : mTypeList,
+        
+          }
+        )
+      }
+    })
+    wx.request({
+      url: 'http://localhost:8527/api/selectByOneself/list?typeID=3',
+      success:function(res)
+      {
+        _this.setData(
+          {
+            dishes : res.data.data
+          }
+        );
+      }
+    })
+  },
+  onTypeTabClick : function(e)
+  {
+    var _this = this
+    var currentType = e.detail.index
+    wx.request({
+      url: 'http://localhost:8527/api/selectByOneself/list?typeID='+currentType,
+      success:function(res)
+      {
+        _this.setData(
+          {
+            dishes : res.data.data
+          }
+        );
+      }
+    })
   }
 
 })
